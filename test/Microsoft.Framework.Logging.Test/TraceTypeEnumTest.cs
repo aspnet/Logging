b7380f9ffef774df44ee1383a2ef475d8118728b
SonjaKhan <sonjak3@uw.edu>
2014-10-01 18:24:18 -0700

    #24 fix TraceType enum values to be consecutive
    #24

---
+++ test/Microsoft.Framework.Logging.Test/TraceTypeEnumTest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.Framework.Logging.Test
+{
+    public class TraceTypeEnumTest
+    {
+        private static readonly List<int> _values;
+
+        static TraceTypeEnumTest()
+        {
+            _values = Enum.GetValues(typeof(TraceType)).Cast<int>().ToList();
+        }
+
+        [Fact]
+        public static void EnumStartsAtOne()
+        {
+            Assert.Equal(_values.Min(), 1);
+        }
+
+        [Fact]
+        public static void EnumValuesAreUniqueAndConsecutive()
+        {
+            var uniqueValues = new HashSet<int>(_values);
+            Assert.Equal(_values.Count, uniqueValues.Count);
+            Assert.Equal(_values.Count, _values.Max() - _values.Min() + 1);
+        }
+    }
+}
