6bb82f11531471e8a082e8305ddeba0016007f1c
SonjaKhan <sonjak3@uw.edu>
2014-10-15 12:11:18 -0700

    moving utility files to the logging repo

---
+++ src/Microsoft.Framework.Logging/Utils/TestLogger.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Framework.Logging
+{
+    public class TestLogger : ILogger
+    {
+        private object _scope;
+        private TestSink _sink;
+        private string _name;
+        private bool _enabled;
+
+        public TestLogger(string name, TestSink sink, bool enabled)
+        {
+            _sink = sink;
+            _name = name;
+            _enabled = enabled;
+        }
+
+        public string Name { get; set; }
+
+        public IDisposable BeginScope(object state)
+        {
+            _scope = state;
+
+            _sink.Begin(new BeginScopeContext()
+            {
+                LoggerName = _name,
+                Scope = state,
+            });
+
+            return NullDisposable.Instance;
+        }
+
+        public void Write(TraceType eventType, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
+        {
+            _sink.Write(new WriteContext()
+            {
+                EventType = eventType,
+                EventId = eventId,
+                State = state,
+                Exception = exception,
+                Formatter = formatter,
+                LoggerName = _name,
+                Scope = _scope
+            });
+        }
+
+        public bool IsEnabled(TraceType eventType)
+        {
+            return _enabled;
+        }
+    }
+}
