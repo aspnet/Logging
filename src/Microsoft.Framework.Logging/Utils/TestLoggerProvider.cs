2c38bd33a6e777542e390822cd3d5ac1772fff6f
SonjaKhan <sonjak3@uw.edu>
2014-12-12 09:31:06 -0800

    modifying logging test classes for compatibility with functional tests

---
+++ src/Microsoft.Framework.Logging/Utils/TestLoggerProvider.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Framework.Logging
+{
+    public class TestLoggerProvider : ILoggerProvider
+    {
+        private readonly TestSink _sink;
+
+        public TestLoggerProvider(TestSink sink)
+        {
+            _sink = sink;
+        }
+
+        public ILogger Create(string name)
+        {
+            return new TestLogger(name, _sink, true);
+        }
+    }
+}
