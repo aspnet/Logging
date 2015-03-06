73287b7f1b2467bd77b38f30c0d75a9321a3af2d
Suhas Joshi <suhasbjoshi@hotmail.com>
2015-03-06 13:59:00 -0800

    Adding IConfiguration overload for console logger

--- src/Microsoft.Framework.Logging.Console/ConsoleLoggerFactoryExtensions.cs
+++ src/Microsoft.Framework.Logging.Console/ConsoleLoggerFactoryExtensions.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel;
+using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.Logging.Console;
 
 namespace Microsoft.Framework.Logging
@@ -33,6 +35,24 @@ namespace Microsoft.Framework.Logging
         public static ILoggerFactory AddConsole(this ILoggerFactory factory, LogLevel minLevel)
         {
             factory.AddProvider(new ConsoleLoggerProvider((category, logLevel) => logLevel >= minLevel));
+            return factory;
+        }
+
+        /// <summary>
+        ///     Adds a console logger that is enabled as defined by the IConfiguration object
+        /// </summary>
+        /// <param name="configuration">IConfiguration object with filter attributes</param>
+        /// <returns></returns>
+        public static ILoggerFactory AddConsole(this ILoggerFactory factory, IConfiguration configuration)
+        {
+            factory.AddProvider(new ConsoleLoggerProvider((category, logLevel) =>
+            {
+                LogLevel minLevel;
+
+                return Enum.TryParse(configuration?.GetSubKey("ConsoleLogger")?.Get(category), out minLevel) &&
+                       logLevel >= minLevel;
+            }));
+
             return factory;
         }
     }
