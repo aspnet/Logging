1683930b0637f580c0856134d2db7e12ad464575
Jesper Lindholm <jesper.lindholm@treetop.se>
2015-03-07 03:52:10 +0100

    Initial prototype of Windows Event Log logger

---
+++ samples/EventLoggingASPNet/Startup.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Text;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Logging.EventLog;
+
+namespace EventLoggingASPNet
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+        
+        // This method gets called by a runtime.
+        // Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services, ILoggerFactory loggerFactory, IHttpContextAccessor httpContextAccessor = null)
+        {
+            loggerFactory.AddWindowsEventLogIfHasPermission(httpContextAccessor, (ex, httpContext, stringBuilder) => {
+                var isSSL = (httpContext.Request.Scheme == "https");
+                stringBuilder.Append("Is SSL/TLS: ");
+                stringBuilder.AppendLine((isSSL ? "Yes" : "No"));
+            });
+            
+            services.AddMvc();
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
---
+++ src/Microsoft.Framework.Logging.EventLog/WindowsEventLogLoggerExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using System;
+using System.Text;
+
+namespace Microsoft.Framework.Logging.EventLog {
+    public static class WindowsEventLogLoggerExtensions {
+        /// <summary>
+        /// Adds a Windows event log logger that is enabled for <see cref="LogLevel"/>.Error or higher and will only log entries with associated exceptions. (If the event source needs to be registered and the host does not run under administrator privileges, the event log is not added.)
+        /// </summary>
+        public static ILoggerFactory AddWindowsEventLogIfHasPermission(this ILoggerFactory factory) {
+            factory.AddProvider(new WindowsEventLogLoggerProvider(doNotThrowOnEventSourceRegistrationFailure: true, httpContextAccessor: null, extraContextInformationLogger:null));
+            return factory;
+        }
+        /// <summary>
+        /// Adds a Windows event log logger that is enabled for <see cref="LogLevel"/>.Error or higher, will only log entries with associated exceptions and will attempt to log information about the active <see cref="HttpContext"/>.
+        /// </summary>
+        public static ILoggerFactory AddWindowsEventLogIfHasPermission(this ILoggerFactory factory, IHttpContextAccessor httpContextAccessor, Action<Exception, HttpContext, StringBuilder> eventLogLoggerExtraContext = null) {
+            factory.AddProvider(new WindowsEventLogLoggerProvider(doNotThrowOnEventSourceRegistrationFailure: true, httpContextAccessor: httpContextAccessor, extraContextInformationLogger: eventLogLoggerExtraContext));
+            return factory;
+        }
+        /// <summary>
+        /// Adds a Windows event log logger that is enabled for <see cref="LogLevel"/>.Error or higher and will only log entries with associated exceptions. (If the event source needs to be registered and the host does not run under administrator privileges, the <see cref="System.Security.SecurityException" /> is propagated.)
+        /// </summary>
+        public static ILoggerFactory AddWindowsEventLogThrowIfMissingPermission(this ILoggerFactory factory) {
+            factory.AddProvider(new WindowsEventLogLoggerProvider(doNotThrowOnEventSourceRegistrationFailure: false, httpContextAccessor: null, extraContextInformationLogger: null));
+            return factory;
+        }
+        /// <summary>
+        /// Adds a Windows event log logger that is enabled for <see cref="LogLevel"/>.Error or higher, will only log entries with associated exceptions and will attempt to log information about the active <see cref="HttpContext"/>. (If the event source needs to be registered and the host does not run under administrator privileges, the <see cref="System.Security.SecurityException" /> is propagated.)
+        /// </summary>
+        public static ILoggerFactory AddWindowsEventLogThrowIfMissingPermission(this ILoggerFactory factory, IHttpContextAccessor httpContextAccessor, Action<Exception, HttpContext, StringBuilder> eventLogLoggerExtraContext = null) {
+            factory.AddProvider(new WindowsEventLogLoggerProvider(doNotThrowOnEventSourceRegistrationFailure: false, httpContextAccessor: httpContextAccessor, extraContextInformationLogger: eventLogLoggerExtraContext));
+            return factory;
+        }
+    }
+}
