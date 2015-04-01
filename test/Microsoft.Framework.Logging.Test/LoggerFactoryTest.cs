170d11b498874e5f6e90f281feab3938bff77560
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2015-04-01 22:15:59 +0200

    Issue #99 : dispose IDisposable ILoggerProvider

---
+++ test/Microsoft.Framework.Logging.Test/LoggerFactoryTest.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Framework.Logging;
+using Xunit;
+
+namespace Microsoft.Framework.Logging.Test
+{
+    public class LoggerFactoryTest
+    {
+        [Fact]
+        public static void Dispose_ProvidersAreDisposed()
+        {
+            // Arrange
+            var factory = new LoggerFactory();
+            var provider = new DisposableProvider();
+            factory.AddProvider(provider);
+
+            // Act
+            factory.Dispose();
+
+            // Assert
+            Assert.True(provider.Disposed);
+        }
+
+        private class DisposableProvider : ILoggerProvider, IDisposable
+        {
+            public bool Disposed { get; set; }
+
+            public ILogger CreateLogger(string name)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+
+        }
+    }
+}
