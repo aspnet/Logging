0e890735e9d065fc18967bd6b2ce84eda90d8f64
Louis DeJardin <lodejard@microsoft.com>
2015-04-20 10:52:47 -0700

    Initial implementation of the Event Bus (INotifier)
    This implementation uses emit to support duck-typing of data for
    notification events on Net45 and DNXCore50.
    
    For now the initial dispatch or 'splat' of event data is done using
    reflection. Next change will update it to also use code generation.

---
+++ src/Microsoft.Framework.Notify/Internal/ProxyBaseOfT.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#if NET45 || DNX451
+
+using Microsoft.Framework.Internal;
+
+namespace Microsoft.Framework.Notify.Internal
+{
+    public class ProxyBase<T> : ProxyBase where T : class
+    {
+        protected T instance;
+
+        public ProxyBase([NotNull] T inst)
+            : base(typeof(T))
+        {
+            this.instance = inst;
+        }
+
+        public T UnderlyingInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public override object UnderlyingInstanceAsObject
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
+
+#endif
