4751f03983cdad6c4ed27b6b6da5021bf7c190bf
Justin Van Patten <jvp@justinvp.com>
2015-06-29 12:56:09 -0700

    Cleanup usings

--- src/Microsoft.Framework.Logging.Console/ConsoleLogger.cs
+++ src/Microsoft.Framework.Logging.Console/ConsoleLogger.cs
@@ -3,9 +3,6 @@
 
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Text;
 using Microsoft.Framework.Logging.Console.Internal;
 
