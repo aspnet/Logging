8dd38a0a8372605510b0a6f8ec7dbb475d71afc8
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2015-07-08 22:05:16 +0200

    Reworked TraceSource  test for Mono

--- test/Microsoft.Framework.Logging.Test/TraceSourceLoggerProviderTest.cs
+++ test/Microsoft.Framework.Logging.Test/TraceSourceLoggerProviderTest.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using Microsoft.Framework.Logging.TraceSource;
 using Xunit;
 
@@ -12,17 +9,14 @@ namespace Microsoft.Framework.Logging.Test
 {
     public class TraceSourceLoggerProviderTest
     {
-        [Theory]
-        [InlineData(true, 3)]
-        [InlineData(false, 1)]
-        public static void Dispose_TraceListenerIsFlushed(bool autoFlush, int expectedFlush)
+        [Fact]
+        public void Dispose_TraceListenerIsFlushedOnce()
         {
             // Arrange
-            Trace.AutoFlush = autoFlush;
             var testSwitch = new SourceSwitch("TestSwitch", "Level will be set to warning for this test");
             testSwitch.Level = SourceLevels.Warning;
             var listener = new BufferedConsoleTraceListener();
-
+            
             TraceSourceLoggerProvider provider = new TraceSourceLoggerProvider(testSwitch, listener);
             var logger1 = provider.CreateLogger("FirstLogger");
             var logger2 = provider.CreateLogger("SecondLogger");
@@ -33,7 +27,7 @@ namespace Microsoft.Framework.Logging.Test
             provider.Dispose();
 
             // Assert
-            Assert.Equal(expectedFlush, listener.FlushCount);
+            Assert.Equal(1, listener.FlushCount);
         }
 
         private class BufferedConsoleTraceListener : ConsoleTraceListener
