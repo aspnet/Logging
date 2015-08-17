9886b92b35d20e34d27d63d07141249ffdbd407e
Adam Hathcock <adam.hathcock@autodesk.com>
2015-08-17 09:25:18 +0100

    Add extension to have CreateLogger with Type object as well as generic

--- src/Microsoft.Framework.Logging.Abstractions/LoggerFactoryExtensions.cs
+++ src/Microsoft.Framework.Logging.Abstractions/LoggerFactoryExtensions.cs
@@ -18,7 +18,16 @@ namespace Microsoft.Framework.Logging
         /// <param name="factory">The factory.</param>
         public static ILogger CreateLogger<T>([NotNull] this ILoggerFactory factory)
         {
-            return factory.CreateLogger(TypeNameHelper.GetTypeDisplayName(typeof(T), fullName: true));
+            return factory.CreateLogger(typeof(T));
+        }
+        /// <summary>
+        /// Creates a new ILogger instance using the full name of the given type.
+        /// </summary>
+        /// <param name="factory">The factory.</param>
+        /// <param name="type">The type.</param>
+        public static ILogger CreateLogger([NotNull] this ILoggerFactory factory, Type type)
+        {
+            return factory.CreateLogger(TypeNameHelper.GetTypeDisplayName(type, fullName: true));
         }
     }
         
