934d27b77b0dc9d329aa1947749b291a8eec1a24
Rahul Kumar <rahku@microsoft.com>
2015-10-06 19:00:25 -0700

    Changes to loggerFactory, add console observer & add sample app2

---
+++ samples/SampleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Framework.Logging;
+using System.Diagnostics.Tracing;
+using Microsoft.Framework.Logging.Observer;
+
+namespace SampleApp2
+{
+    public class Program
+    {
+        private readonly Logger _logger;
+
+        public Program()
+        {
+            // a DI based application would get ILoggerFactory injected instead
+            var factory = new LoggerFactory();
+
+            // getting the logger immediately using the class's name is conventional
+            _logger = factory.CreateSystemLogger(typeof(Program).FullName);
+
+            var observer = new ConsoleObserver();
+
+            factory.Subscribe(observer);
+
+        }
+
+        public void Main(string[] args)
+        {
+            _logger.Log(System.Diagnostics.Tracing.LogLevel.Informational, "InfoMessage", "Starting");
+
+            var startTime = DateTimeOffset.UtcNow;
+            _logger.Log(System.Diagnostics.Tracing.LogLevel.Informational, "InfoMessage", new { startTime = startTime, hex = 0x42 });
+
+
+            try
+            {
+                throw new Exception("Boom");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(System.Diagnostics.Tracing.LogLevel.Critical, "CriticalMessage", ex);
+
+                // This write should not log anything
+                _logger.Log(System.Diagnostics.Tracing.LogLevel.Critical, "CriticalMessage", null);
+
+                _logger.Log(System.Diagnostics.Tracing.LogLevel.Error, "CriticalMessage", ex);
+                _logger.Log(System.Diagnostics.Tracing.LogLevel.Warning, "CriticalMessage", ex);
+            }
+
+
+            using (_logger.ActivityStart(System.Diagnostics.Tracing.LogLevel.Informational, "Main", "Main"))
+            {
+                Console.WriteLine("Hello World");
+
+                _logger.Log(System.Diagnostics.Tracing.LogLevel.Informational, "InfoMessage", "Waiting for user input");
+                Console.ReadLine();
+            }
+
+
+
+            var endTime = DateTimeOffset.UtcNow;
+            _logger.Log(System.Diagnostics.Tracing.LogLevel.Informational, "InfoMessage", new { StopTime = endTime });
+
+            _logger.Log(System.Diagnostics.Tracing.LogLevel.Informational, "InfoMessage", "Stopping");
+
+            _logger.Log(System.Diagnostics.Tracing.LogLevel.Informational, "InfoMessage", Environment.NewLine);
+            _logger.Log(System.Diagnostics.Tracing.LogLevel.Informational, "InfoMessage", new { Result = "SUCCESS", StartTime = startTime, EndTime = endTime, Duration = (endTime - startTime).TotalMilliseconds });
+        }
+    }
+}
--- src/Microsoft.Framework.Logging/LoggerFactory.cs
+++ src/Microsoft.Framework.Logging/LoggerFactory.cs
@@ -4,18 +4,54 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics.Tracing;
 
 namespace Microsoft.Framework.Logging
 {
     /// <summary>
     /// Summary description for LoggerFactory
     /// </summary>
-    public class LoggerFactory : ILoggerFactory
+    public class LoggerFactory : ILoggerFactory, IObservable<KeyValuePair<string, object>>, IDisposable
     {
         private readonly Dictionary<string, Logger> _loggers = new Dictionary<string, Logger>(StringComparer.Ordinal);
         private ILoggerProvider[] _providers = new ILoggerProvider[0];
         private readonly object _sync = new object();
         private bool _disposed = false;
+        private readonly Dictionary<string, System.Diagnostics.Tracing.Logger> _systemLoggers = new Dictionary<string, System.Diagnostics.Tracing.Logger>(StringComparer.Ordinal);
+        private List<IObserver<KeyValuePair<string, object>>> _observers = new List<IObserver<KeyValuePair<string, object>>>();
+
+        public System.Diagnostics.Tracing.Logger CreateSystemLogger(string categoryName)
+        {
+            System.Diagnostics.Tracing.Logger logger;
+            lock (_sync)
+            {
+                if (!_systemLoggers.TryGetValue(categoryName, out logger))
+                {
+                    logger = new System.Diagnostics.Tracing.Logger(categoryName);
+                    _systemLoggers[categoryName] = logger;
+                    //TODO create an observer which will pipe messages from sysLogger to Logger;
+                    _loggers[categoryName] = new Logger(this, categoryName);
+
+                    // subscribe all existing observers to the new logger
+                    foreach (var observer in _observers)
+                        //TODO IDisposable is lost
+                        logger.Subscribe(observer);
+                }
+            }
+            return logger;
+        }
+
+        public IDisposable Subscribe(IObserver<KeyValuePair<string, object>> observer)
+        {
+            List<IDisposable> subsribers = new List<IDisposable>();
+            lock (_sync)
+            {
+                _observers.Add(observer);
+                foreach (var logger in _systemLoggers.Values)
+                    subsribers.Add(logger.Subscribe(observer, System.Diagnostics.Tracing.LogLevel.Verbose));
+            }
+            return new DisposeAll(subsribers);
+        }
 
         public ILogger CreateLogger(string categoryName)
         {
@@ -66,8 +102,36 @@ namespace Microsoft.Framework.Logging
                     }
                 }
 
+                if (_systemLoggers != null)
+                {
+                    foreach (var logger in _systemLoggers.Values)
+                        logger.Dispose();
+                    _systemLoggers.Clear();
+                }
+
                 _disposed = true;
             }
+        }
+
+        private class DisposeAll : IDisposable
+        {
+            public DisposeAll(List<IDisposable> toDispose)
+            {
+                _toDispose = toDispose;
+
+            }
+
+            public void Dispose()
+            {
+                var toDispose = _toDispose;
+                if (toDispose != null)
+                {
+                    _toDispose = null;
+                    foreach (var subscription in toDispose)
+                        subscription.Dispose();
+                }
+            }
+            List<IDisposable> _toDispose;
         }
     }
 }
