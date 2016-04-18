b2ad5dfd8b6e8a079ff3df8ba5969a32d52b17ec
Ben Adams <thundercat@illyriad.co.uk>
2016-04-18 09:29:08 +0100

    Cache the color changes

--- src/Microsoft.Extensions.Logging.Console/Internal/WindowsLogConsole.cs
+++ src/Microsoft.Extensions.Logging.Console/Internal/WindowsLogConsole.cs
@@ -7,19 +7,24 @@ namespace Microsoft.Extensions.Logging.Console.Internal
 {
     public class WindowsLogConsole : IConsole
     {
+        private ConsoleColor _background = System.Console.BackgroundColor;
+        private ConsoleColor _foreground = System.Console.ForegroundColor;
+
         public void Write(string message, ConsoleColor? background, ConsoleColor? foreground)
         {
             var changedColor = false;
-            if (background.HasValue)
+            if (background.HasValue && _background != background)
             {
                 System.Console.BackgroundColor = background.Value;
                 changedColor = true;
+                _background = background.Value;
             }
 
             if (foreground.HasValue)
             {
                 System.Console.ForegroundColor = foreground.Value;
                 changedColor = true;
+                _foreground = foreground.Value;
             }
 
             System.Console.Write(message);
