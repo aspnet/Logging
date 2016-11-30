90135122b568d05c839cb45922be8fa754cca9b7
Sly Gryphon <sgryphon@computer.org>
2016-11-30 15:45:58 +1000

    Add LoggingEvents static to console hierarchy sample with some generic definitions, to show theory of event IDs.

---
+++ samples/ConsoleHierarchySample/LoggingEvents.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConsoleHierarchySample
+{
+    /// <summary>
+    /// Sample generic logging event IDs. 
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// NOTE: This enum is provided as an example only; it is recommended that each applications
+    /// use their own custom logging event IDs.
+    /// </para>
+    /// <para>
+    /// The structure of the event IDs are based on a structure similar to the theory of 
+    /// response codes used for Internet systems such as SMTP and HTTP.
+    /// </para>
+    /// <para>
+    /// In this case; the event IDs are based on a 4 digit system.
+    /// </para>
+    /// <para>
+    /// The first digit is the class of the event: 
+    /// 1 for startup code; 2 for events that have occurred (completed); 
+    /// 3 for actions the program initiates; 4 for warnings; 5 for errors; 8 for shutdown or
+    /// end events; and 9 for unknown or critical issues.
+    /// </para>
+    /// <para>
+    /// The class is similar to the event level (e.g. events starting with 5 will usually
+    /// be logged at the Error level).
+    /// </para>
+    /// <para>
+    /// The second digit is the area of the program: 0 for configuration or syntax; 1 for
+    /// system level; 2 for connnections; 3 for security and authentication; and 9 for 
+    /// an unknown area; such as an unhandled general exception.
+    /// </para>
+    /// <para>
+    /// Applications can use the second digit 4-8 for the major subsystems; which will
+    /// vary between applications.
+    /// </para>
+    /// </remarks>
+    public static class LoggingEvents
+    {
+        public static EventId ConfigurationStart = 1000;
+        public static EventId SystemStart = 1100;
+        public static EventId ConnectionStart = 1200;
+
+        public static EventId SystemEvent = 2100;
+        public static EventId ConnectionEvent = 2200;
+        public static EventId AuthenticationSuccess = 2300;
+
+        public static EventId ConfigurationAction = 3000;
+        public static EventId SystemAction = 3100;
+        public static EventId ConnectionAction = 3200;
+
+        public static EventId ConfigurationWarning = 4000;
+        public static EventId SystemWarning = 4100;
+        public static EventId ConnectionWarning = 4200;
+        public static EventId AuthenticationFailure = 4300;
+
+        public static EventId ConfigurationError = 5000;
+        public static EventId SystemError = 5100;
+        public static EventId ConnectionError = 5200;
+        public static EventId AuthenticationError = 5300;
+        public static EventId UnknownError = 5900;
+
+        public static EventId SystemStop = 8100;
+        public static EventId ConnectionStop = 8200;
+
+        public static EventId ConfigurationCriticalError = 9000;
+        public static EventId SystemCriticalError = 9100;
+        public static EventId ConnectionCriticalError = 9200;
+        public static EventId AuthenticationCriticalError = 9300;
+        public static EventId UnknownCriticalError = 9900;
+    }
+}
--- samples/ConsoleHierarchySample/Program.cs
+++ samples/ConsoleHierarchySample/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using System;
+using System.Collections.Generic;
 
 namespace ConsoleHierarchySample
 {
@@ -19,7 +17,7 @@ namespace ConsoleHierarchySample
 
             var factory = new LoggerFactory();
 
-            // Qhen console settings are defined, then only the categories listed are logged;
+            // When console settings are defined, then only the categories listed are logged;
             // other categories default to the value of the special 'Default' (case sensitive) 
             // category, or if not specified then they are not logged at all.
             var consoleSettings = new ConsoleLoggerSettings();
@@ -31,54 +29,61 @@ namespace ConsoleHierarchySample
             factory.AddConsole(consoleSettings);
 
             // Normally these are created by an injected ILogger<T> and automatically extract the full class name.
+            // Loggers at other levels (e.g. namespace) would need to be created manually.
             var logger_A_1 = factory.CreateLogger("CompanyA.Namespace1");
             var logger_A_1_a = factory.CreateLogger("CompanyA.Namespace1.ClassA");
             var logger_A_1_b = factory.CreateLogger("CompanyA.Namespace1.ClassB");
-            var logger_A_2 = factory.CreateLogger("CompanyA.Namespace2");
             var logger_A_2_c = factory.CreateLogger("CompanyA.Namespace2.ClassC");
+            var logger_B_3 = factory.CreateLogger("CompanyB.Namespace3");
             var logger_B_3_d = factory.CreateLogger("CompanyB.Namespace3.ClassD");
 
-            logger_A_1.LogInformation(1001, "A_1 Information");
-            logger_A_1_a.LogInformation(1101, "A_1_a Information");
-            logger_A_1_b.LogInformation(1201, "A_1_b Information");
-            logger_A_2.LogInformation(1301, "A_2 Information");
-            logger_A_2_c.LogInformation(1401, "A_2_c Information");
-            logger_B_3_d.LogInformation(1501, "B_3_d Information");
+            logger_A_1.LogInformation(LoggingEvents.ConfigurationStart, "A_1 Information - namespace level Configuration Start");
+            logger_A_1_a.LogInformation(LoggingEvents.SystemStart, "A_1_a Information - System Start");
+            logger_A_1_b.LogInformation(LoggingEvents.SystemEvent, "A_1_b Information - System Event");
+            logger_A_2_c.LogInformation(LoggingEvents.ConnectionEvent, "A_2_c Information - Connection Event");
+            logger_B_3.LogInformation(2301, "B_3 Information - namespace level");
+            logger_B_3_d.LogInformation(LoggingEvents.AuthenticationSuccess, "B_3_d Information - Authentication Success");
 
-            logger_A_1.LogDebug("A_1 Debug");
-            logger_A_1_a.LogDebug("A_1_a Debug");
-            logger_A_1_b.LogDebug("A_1_b Debug");
-            logger_A_2.LogDebug("A_2 Debug");
-            logger_A_2_c.LogDebug("A_2_c Debug");
-            logger_B_3_d.LogDebug("B_3_d Debug");
-
-            logger_A_1.LogInformation(1002, "A_1 Information 2");
-            logger_A_1_a.LogInformation(1102, "A_1_a Information 2");
-            logger_A_1_b.LogInformation(1202, "A_1_b Information 2");
-            logger_A_2.LogInformation(1302, "A_2 Information 2");
-            logger_A_2_c.LogInformation(1402, "A_2_c Information 2");
-            logger_B_3_d.LogInformation(1502, "B_3_d Information 2");
-
-            logger_A_1.LogDebug("A_1 Debug 2");
-            logger_A_1_a.LogDebug("A_1_a Debug 2");
-            logger_A_1_b.LogDebug("A_1_b Debug 2");
-            logger_A_2.LogDebug("A_2 Debug 2");
-            logger_A_2_c.LogDebug("A_2_c Debug 2");
-            logger_B_3_d.LogDebug("B_3_d Debug 2");
-
-            logger_A_1.LogWarning(4001, "A_1 Warning");
-            logger_A_1_a.LogWarning(4101, "A_1_a Warning");
-            logger_A_1_b.LogError(5201, "A_1_b Error");
-            logger_A_2.LogWarning(4301, "A_2 Warning");
-            logger_A_2_c.LogWarning(4401, "A_2_c Warning");
             try
             {
-                throw new Exception("Sample");
+                logger_A_1.LogDebug("A_1 Debug");
+                logger_A_1_a.LogDebug("A_1_a Debug - Class A detail");
+                logger_A_1_b.LogDebug("A_1_b Debug - Class B detail");
+                logger_A_2_c.LogDebug("A_2_c Debug");
+                logger_B_3.LogDebug("A_2 Debug");
+                logger_B_3_d.LogDebug("B_3_d Debug");
+
+                logger_A_1_a.LogDebug("A_1_a Debug 2 - Class A more detail");
+                logger_A_1_b.LogDebug("A_1_b Debug 2 - Class B more detail");
+                logger_A_2_c.LogDebug("A_2_c Debug 2");
+                logger_B_3_d.LogDebug("B_3_d Debug 2");
+
+                logger_A_1_a.LogDebug("A_1_a Debug 3 - Class A even more detail");
+                logger_A_1_b.LogDebug("A_1_b Debug 3 - Class B even more detail");
+
+                logger_A_1_a.LogDebug("A_1_a Debug 4 - Class A still going");
+                logger_A_1_b.LogDebug("A_1_b Debug 4 - Class B still going");
+
+                logger_A_1.LogWarning(LoggingEvents.ConfigurationWarning, "A_1 Warning - namespace level Configuration Warning");
+                logger_A_1_a.LogWarning(LoggingEvents.SystemWarning, "A_1_a Warning - System Warning");
+                logger_A_1_b.LogError(LoggingEvents.SystemError, "A_1_b Error - System Error");
+                logger_A_2_c.LogWarning(LoggingEvents.ConnectionWarning, "A_2_c Warning - Connection Warning");
+
+                TestExceptionLogging();
             }
             catch (Exception ex)
             {
-                logger_B_3_d.LogCritical(9501, ex, "B_3_d Critical");
+                logger_B_3_d.LogCritical(LoggingEvents.AuthenticationCriticalError, ex, "B_3_d Critical = Authentication Critical Error");
             }
+
+            logger_A_2_c.LogInformation(LoggingEvents.ConnectionStop, "B_3_d Information 2 - Connection Stop");
+            logger_A_1_b.LogInformation(LoggingEvents.SystemStop, "A_1_b Information 2 - System Stop");
+            logger_A_1_a.LogInformation(6102, "A_1_a Information 2");
+        }
+
+        private static void TestExceptionLogging()
+        {
+            throw new NotImplementedException();
         }
     }
 }
