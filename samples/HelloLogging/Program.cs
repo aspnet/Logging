ce46ddc7ae9b03a23339fc152993d4bb53b3a320
Sly Gryphon <sgryphon@computer.org>
2016-11-30 13:30:26 +1000

    Very simple, basic "hello world", example for logging.

---
+++ samples/HelloLogging/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace HelloLogging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new LoggerFactory();
+            factory.AddConsole();
+            var logger = factory.CreateLogger<Program>();
+            logger.LogInformation("Hello logging");
+        }
+    }
+}
