ae636076e327e49281b37e1d33b8a957959753b2
Sly Gryphon <sgryphon@computer.org>
2016-12-06 22:20:04 +1000

    Move sample classes to separate file; add copyright header

--- samples/ConsoleHierarchySample/Program.cs
+++ samples/ConsoleHierarchySample/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using System;
 using System.Collections.Generic;
@@ -85,26 +88,5 @@ namespace ConsoleHierarchySample
         {
             throw new NotImplementedException();
         }
-    }
-}
-
-namespace CompanyA
-{
-    namespace Namespace1
-    {
-        class ClassA { }
-        class ClassB { }
-    }
-    namespace Namespace2
-    {
-        class ClassC { }
-    }
-}
-
-namespace CompanyB
-{
-    namespace Namespace3
-    {
-        class ClassD { }
     }
 }
