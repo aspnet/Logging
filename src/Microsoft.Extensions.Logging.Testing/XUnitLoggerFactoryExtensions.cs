02c29b8b40a1dd8b5dc998caa2fd3e45669ca45c
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-03-16 15:12:43 -0700

    add xunit logger provider
    fixes #574

---
+++ src/Microsoft.Extensions.Logging.Testing/XUnitLoggerFactoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Logging.Testing;
+using Xunit.Abstractions;
+
+namespace Microsoft.Extensions.Logging
+{
+    public static class XUnitLoggerFactoryExtensions
+    {
+        public static void AddXUnit(this ILoggerFactory loggerFactory, ITestOutputHelper output)
+        {
+            loggerFactory.AddProvider(new XUnitLoggerProvider(output));
+        }
+
+        public static void AddXUnit(this ILoggerFactory loggerFactory, ITestOutputHelper output, LogLevel minLevel)
+        {
+            loggerFactory.AddProvider(new XUnitLoggerProvider(output, minLevel));
+        }
+    }
+}
