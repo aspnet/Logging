1f69a7ef6bfdafed58b3cbfea7ab1a9f61f71b4e
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-04-18 11:30:20 -0700

    copy test logging stuff from IntegrationTesting here

---
+++ src/Microsoft.Extensions.Logging.Testing/LoggedTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xunit.Abstractions;
+
+namespace Microsoft.Extensions.Logging.Testing
+{
+    public abstract class LoggedTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public LoggedTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public IDisposable StartLog(out ILoggerFactory loggerFactory, [CallerMemberName] string testName = null)
+        {
+            return AssemblyTestLog.ForAssembly(GetType().GetTypeInfo().Assembly).StartTestLog(_output, GetType().FullName, out loggerFactory, testName);
+        }
+    }
+}
