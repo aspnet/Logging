cc54a6fe7e00d90a8ae770f139a6c40a57e188c0
Pavel Krymets <pavel@krymets.com>
2017-07-05 10:47:02 -0700

    Using

--- src/Microsoft.Extensions.Logging/LoggingBuilderExtensions.cs
+++ src/Microsoft.Extensions.Logging/LoggingBuilderExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.Logging
