c960c57235c7a6974ea72ade809bed5904093810
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2018-03-24 16:16:29 -0700

    extend support to LoggerMessage.Define

--- test/Microsoft.Extensions.Logging.Analyzer.Test/FormatStringAnalyzerTests.cs
+++ test/Microsoft.Extensions.Logging.Analyzer.Test/FormatStringAnalyzerTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Logging.Analyzers;
 using Xunit;
@@ -12,60 +13,90 @@ namespace Microsoft.Extensions.Logging.Analyzer.Test
     public class FormatStringAnalyzerTests : DiagnosticVerifier
     {
         [Theory]
-        [MemberData(nameof(GenerateTemplateUsages), @"""{0}"", 1")]
-        public void DiagnosticIsProducedForNumericFormatArgument(string format)
+        [MemberData(nameof(GenerateTemplateUsages), @"""{0}""", "1", true)]
+        public void MEL0001IsProducedForNumericFormatArgument(string format)
         {
-            var diagnostic = Assert.Single(GetDiagnostics(format));
+            // Enable MEL0005 because it shouldn't trigger on numeric arguments and we want to verify that.
+            var diagnostic = Assert.Single(GetDiagnostics(format, "MEL0005"));
             Assert.Equal("MEL0001", diagnostic.Id);
         }
 
         [Theory]
-        [MemberData(nameof(GenerateTemplateUsages), @"""{camelCase}"", 1")]
-        public void DiagnosticIsProducedForCamelCasedFormatArgument(string format)
-        {
-            var diagnostic = Assert.Single(GetDiagnostics(format, "MEL0005"));
-            Assert.Equal("MEL0005", diagnostic.Id);
-        }
-
-        [Theory]
-        [MemberData(nameof(GenerateTemplateUsages), @"$""{string.Empty}""")]
-        [MemberData(nameof(GenerateTemplateUsages), @"""string"" + 2")]
-        public void DiagnosticIsProducedForDynamicFormatArgument(string format)
+        [MemberData(nameof(GenerateTemplateUsages), @"$""{string.Empty}""", "", true)]
+        [MemberData(nameof(GenerateTemplateUsages), @"""string"" + 2", "", true)]
+        public void MEL0002IsProducedForDynamicFormatArgument(string format)
         {
             var diagnostic = Assert.Single(GetDiagnostics(format));
             Assert.Equal("MEL0002", diagnostic.Id);
         }
 
         [Theory]
-        [MemberData(nameof(GenerateTemplateUsages), @"""{string}"", 1, 2")]
-        [MemberData(nameof(GenerateTemplateUsages), @"""{str"" + ""ing}"", 1, 2")]
-        [MemberData(nameof(GenerateTemplateUsages), @"""{"" + nameof(ILogger) + ""}""")]
-        [MemberData(nameof(GenerateTemplateUsages), @"""{"" + Const + ""}""")]
-        public void DiagnosticIsProducedForFormatArgumentCountMismatch(string format)
+        [MemberData(nameof(GenerateTemplateUsages), @"""{string}""", "1, 2", false)]
+        [MemberData(nameof(GenerateTemplateUsages), @"""{str"" + ""ing}""", "1, 2", false)]
+        [MemberData(nameof(GenerateTemplateUsages), @"""{"" + nameof(ILogger) + ""}""", "", false)]
+        [MemberData(nameof(GenerateTemplateUsages), @"""{"" + Const + ""}""", "", false)]
+        public void MEL0003IsProducedForFormatArgumentCountMismatch(string format)
         {
             var diagnostic = Assert.Single(GetDiagnostics(format));
             Assert.Equal("MEL0003", diagnostic.Id);
         }
 
         [Theory]
+        [InlineData(@"LoggerMessage.Define<int>(LogLevel.Information, 42, ""{One} {Two} {Three}"");")]
+        [InlineData(@"LoggerMessage.Define<int, int, int, int>(LogLevel.Information, 42, ""{One} {Two} {Three}"");")]
+        public void MEL0003IsProducedForDefineMessageTypeParameterMismatch(string invocation)
+        {
+            var diagnostic = Assert.Single(GetDiagnostics(invocation));
+            Assert.Equal("MEL0003", diagnostic.Id);
+        }
+
+        [Theory]
+        [InlineData("LogTrace", @"""This is a test {Message}"", ""Foo""")]
+        [InlineData("LogDebug", @"""This is a test {Message}"", ""Foo""")]
+        [InlineData("LogInformation", @"""This is a test {Message}"", ""Foo""")]
+        [InlineData("LogWarning", @"""This is a test {Message}"", ""Foo""")]
+        [InlineData("LogError", @"""This is a test {Message}"", ""Foo""")]
+        [InlineData("LogCritical", @"""This is a test {Message}"", ""Foo""")]
+        [InlineData("BeginScope", @"""This is a test {Message}"", ""Foo""")]
+        public void MEL0004IsProducedForInvocationsOfAllLoggerExtensions(string method, string args)
+        {
+            var diagnostic = Assert.Single(GetDiagnostics($"logger.{method}({args});", args, "MEL0004"));
+            Assert.Equal("MEL0004", diagnostic.Id);
+            Assert.Equal($"For improved performance, use pre-compiled log messages instead of calling '{method}' with a string message.", diagnostic.GetMessage());
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateTemplateUsages), @"""{camelCase}""", "1", true)]
+        public void MEL0005IsProducedForCamelCasedFormatArgument(string format)
+        {
+            var diagnostic = Assert.Single(GetDiagnostics(format, "MEL0005"));
+            Assert.Equal("MEL0005", diagnostic.Id);
+        }
+
+        [Theory]
         // Concat would be optimized by compiler
-        [MemberData(nameof(GenerateTemplateUsages), @"nameof(ILogger) + "" string""")]
-        [MemberData(nameof(GenerateTemplateUsages), @""" string"" + "" string""")]
-        [MemberData(nameof(GenerateTemplateUsages), @"$"" string"" + $"" string""")]
-        [MemberData(nameof(GenerateTemplateUsages), @"""{st"" + ""ring}"", 1")]
+        [MemberData(nameof(GenerateTemplateUsages), @"nameof(ILogger) + "" string""", "", true)]
+        [MemberData(nameof(GenerateTemplateUsages), @""" string"" + "" string""", "", true)]
+        [MemberData(nameof(GenerateTemplateUsages), @"$"" string"" + $"" string""", "", true)]
+        [MemberData(nameof(GenerateTemplateUsages), @"""{st"" + ""ring}""", "1", true)]
 
         // we are unable to parse expressions
-        [MemberData(nameof(GenerateTemplateUsages), @"""{string} {string}"", new object [] {1}")]
+        [MemberData(nameof(GenerateTemplateUsages), @"""{string} {string}""", "new object[] { 1 }", true)]
 
-        // MEL0005 is not enabled.
-        [MemberData(nameof(GenerateTemplateUsages), @"""{camelCase}"", 1")]
-        public void DiagnosticNotIsProduced(string format)
+        // MEL0005 is not enabled by default.
+        [MemberData(nameof(GenerateTemplateUsages), @"""{camelCase}""", "1", true)]
+        public void TemplateDiagnosticsAreNotProduced(string format)
         {
             Assert.Empty(GetDiagnostics(format));
         }
 
-        public static IEnumerable<object[]> GenerateTemplateUsages(string templateAndArguments)
+        public static IEnumerable<object[]> GenerateTemplateUsages(string template, string arguments, bool includeDefineMessage)
         {
+            var templateAndArguments = template;
+            if (!string.IsNullOrEmpty(arguments))
+            {
+                templateAndArguments = $"{template}, {arguments}";
+            }
             var methods = new[] { "LogTrace", "LogError", "LogWarning", "LogInformation", "LogDebug", "LogCritical" };
             var formats = new[]
             {
@@ -83,6 +114,25 @@ namespace Microsoft.Extensions.Logging.Analyzer.Test
             }
 
             yield return new[] { $"logger.BeginScope({templateAndArguments});" };
+
+            // This is super rudimentary, but it works
+            if (includeDefineMessage)
+            {
+                var braceCount = template.Count(c => c == '{');
+                yield return new[] { $"LoggerMessage.{GenerateGenericInvocation(braceCount, "DefineScope")}({template});" };
+                yield return new[] { $"LoggerMessage.{GenerateGenericInvocation(braceCount, "Define")}(LogLevel.Information, 42, {template});" };
+            }
+        }
+
+        private static string GenerateGenericInvocation(int i, string method)
+        {
+            if (i > 0)
+            {
+                var types = string.Join(", ", Enumerable.Range(0, i).Select(_ => "int"));
+                method += $"<{types}>";
+            }
+
+            return method;
         }
 
         private static Diagnostic[] GetDiagnostics(string expression, params string[] additionalEnabledDiagnostics)
@@ -99,7 +149,7 @@ public class Program
     }}
 }}
 ";
-            return GetSortedDiagnostics(new[] { code }, new LogFormatAnalyzer(), additionalEnabledDiagnostics);
+            return GetSortedDiagnosticsAsync(new[] { code }, new LogFormatAnalyzer(), additionalEnabledDiagnostics).Result;
         }
     }
 }
