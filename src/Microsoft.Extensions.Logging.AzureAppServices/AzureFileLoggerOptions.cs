0276b60d73814b984e4ce220735e7a14ccfe933b
Xavier Hahn <xavier.hahn@gmail.com>
2018-10-06 21:47:49 +0200

    Change the FileName documentation to make it clear that it's a prefix

--- src/Microsoft.Extensions.Logging.AzureAppServices/AzureFileLoggerOptions.cs
+++ src/Microsoft.Extensions.Logging.AzureAppServices/AzureFileLoggerOptions.cs
@@ -51,8 +51,8 @@ namespace Microsoft.Extensions.Logging.AzureAppServices
         }
 
         /// <summary>
-        /// Gets or sets a string representing the name of the file used to store the logging information.
-        /// The current date, in the format YYYYMMDD will be added after the given filename.
+        /// Gets or sets a string representing the prefix of the file name used to store the logging information.
+        /// The current date, in the format YYYYMMDD will be added after the given value.
         /// Defaults to <c>diagnostics-</c>.
         /// </summary>
         public string FileName
